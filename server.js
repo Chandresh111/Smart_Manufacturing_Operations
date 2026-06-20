d72b9fdfc388b053d639ac9799c2b911ad49eace
Chandresh111 <chandreshdubey79@gmail.com>
2026-06-20 22:32:23 +0530

    Standardize all ports to 5000

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-// Set to 5001 to match your Kubernetes deployment.yaml
-const PORT = process.env.PORT || 5001;
+// Set to 5000 to match your Kubernetes deployment.yaml
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
